6a4393618019b0703a01551d1cba0277635530f9
Isa Dawud <171198688+IsaAmirDawud@users.noreply.github.com>
2025-03-17 21:54:06 -0400

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,9 +1,8 @@
-const createNotesBtn = document.getElementById("create-note-btb");
+const createNotesBtn = document.getElementById("create-note-btn");
 const notesContainer = document.getElementById("notes-container");
 const title = document.getElementById("title");
 
 createNotesBtn.addEventListener("click", ()=>{
-    /*notesContainer.innerHTML += `
-    <textarea></textarea>`;*/
-    
+    notesContainer.innerHTML += `
+    <textarea></textarea>`;
 })
