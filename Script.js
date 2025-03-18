3e8f0ff374f7e102eadea2be76fa82e7b85b9ccf
Isa Dawud <171198688+IsaAmirDawud@users.noreply.github.com>
2025-03-17 21:46:30 -0400

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,9 @@
+const createNotesBtn = document.getElementById("create-note-btb");
+const notesContainer = document.getElementById("notes-container");
+const title = document.getElementById("title");
+
+createNotesBtn.addEventListener("click", ()=>{
+    /*notesContainer.innerHTML += `
+    <textarea></textarea>`;*/
+    
+})
