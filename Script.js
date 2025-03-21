ad6ae3ca203f48809eae08e306ae1ed8857fa9e1
Isa Dawud <171198688+IsaAmirDawud@users.noreply.github.com>
2025-03-20 21:23:58 -0400

    Delete Script.js

--- Script.js
+++
@@ -1,8 +0,0 @@
-const createNotesBtn = document.getElementById("create-note-btn");
-const notesContainer = document.getElementById("notes-container");
-const title = document.getElementById("title");
-
-createNotesBtn.addEventListener("click", ()=>{
-    notesContainer.innerHTML += `
-    <textarea></textarea>`;
-})
